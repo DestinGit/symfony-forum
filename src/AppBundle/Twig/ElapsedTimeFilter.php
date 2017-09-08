47cf968cc7b9c3385d1830b1dd0532a00abb8817
formation <bak1tino@gmail.com>
2017-09-08 16:46:58 +0200

    Création d'une extension twig

---
+++ src/AppBundle/Twig/ElapsedTimeFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Twig;
+
+
+class ElapsedTimeFilter extends \Twig_Extension
+{
+    private $intervalFormat = [
+        'y' => 'an',
+        'm' => 'mois',
+        'd' => 'mois',
+        'h' => 'heure',
+        'i' => 'minute',
+        's' => 'seconde'
+    ];
+
+    public function getName() {
+        return 'elapsedTimeFilter';
+    }
+
+    public function getFilters() {
+        return [
+            new \Twig_SimpleFilter('elapsed', [$this, 'elapsed'])
+        ];
+    }
+
+    public function elapsed($date) {
+        $now = new \DateTime();
+
+        $interval = $now->diff($date);
+
+        $format = '';
+
+        foreach ($this->intervalFormat as $key=>$val) {
+            $value = $interval->$key;
+            if ($value > 0) {
+                $format .= "%{$key} $val ";
+            }
+        }
+        return $interval->format($format);
+    }
+}
